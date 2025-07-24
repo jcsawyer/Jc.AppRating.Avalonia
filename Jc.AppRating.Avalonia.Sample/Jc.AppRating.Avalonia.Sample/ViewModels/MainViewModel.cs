935cf48a84bdc8dddccb378edaa9247df118aa63
Joseph Sawyer <joseph@jcsawyer.com>
2025-07-25 00:37:43 +0100

    feat: add samples, refactor a little and add docs

---
+++ Jc.AppRating.Avalonia.Sample/Jc.AppRating.Avalonia.Sample/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace Jc.AppRating.Avalonia.Sample.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public ICommand InAppRatingCommand { get; }
+    public ICommand InStoreRatingCommand { get; }
+
+    public MainViewModel()
+    {
+        InAppRatingCommand = ReactiveCommand.CreateFromTask(InAppRating);
+        InStoreRatingCommand = ReactiveCommand.CreateFromTask(InStoreRating);
+    }
+
+    private async Task InAppRating()
+    {
+        await AppRating.Current.RequestInAppRatingAsync();
+    }
+
+    private async Task InStoreRating()
+    {
+        if (OperatingSystem.IsAndroid())
+        {
+            await AppRating.Current.RequestInStoreRatingAsync("com.taptapremote.android");
+        }
+        else if (OperatingSystem.IsIOS())
+        {
+            await AppRating.Current.RequestInStoreRatingAsync("com.taptapremote.ios");
+        }
+    }
+}
